441851e2cba9b7ec447270b658a0c85d412d1e89
Maxime Maheo <imaxame@gmail.com>
2017-03-20 17:22:59 +0100

    add bootstrap

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by mmaheo on 09/03/2017.
  */
 @SpringBootApplication
 @ComponentScan({"controller"})
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
