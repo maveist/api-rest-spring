afa6362a6aed439ba6eaddb6430c80ef5e32bfe5
Maxime Maheo <imaxame@gmail.com>
2017-03-23 15:57:32 +0100

    Ajouter et supprimer un collaborateur

--- src/main/java/controller/WorkerController.java
+++ src/main/java/controller/WorkerController.java
@@ -1,18 +1,17 @@
 package controller;
 
 import model.Skill;
+import model.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ResourceNotFoundException;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import repository.SkillRepository;
+import repository.WorkerRepository;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,37 +20,36 @@ import java.util.List;
  * Created by mmaheo on 09/03/2017.
  */
 @Controller
-@RequestMapping("/skills")
+@RequestMapping("/workers")
 @EnableMongoRepositories("repository")
-public class SkillController extends WebMvcConfigurerAdapter {
+public class WorkerController extends WebMvcConfigurerAdapter {
 
     @Autowired
-    private SkillRepository repository;
+    private WorkerRepository repository;
 
     @GetMapping("")
     public ModelAndView retrieve() {
-        List<Skill> skills = repository.findAll(new Sort(Sort.Direction.ASC, "name"));
-        ModelAndView model = new ModelAndView("skill/index");
-        model.addObject("skills", skills);
+        List<Worker> workers = repository.findAll();
+        ModelAndView model = new ModelAndView("worker/index");
+        model.addObject("workers", workers);
 
         return model;
     }
 
     @GetMapping("/add")
-    public String showForm(Skill skill) {
-        return "skill/create";
+    public String showForm(Worker skill) {
+        return "worker/create";
     }
 
     @RequestMapping("/{id}")
     public
     @ResponseBody
     ModelAndView showDetails(@PathVariable(value = "id") String id) {
-        Skill skill = repository.findOne(id);
-        ModelAndView model = new ModelAndView("skill/details");
+        Worker worker = repository.findOne(id);
+        ModelAndView model = new ModelAndView("worker/details");
 
-        if (skill != null) {
-            model.addObject("skill", skill);
-            model.addObject("ressources", skill.getRessources());
+        if (worker != null) {
+            model.addObject("worker", worker);
             return model;
         }
 
@@ -65,18 +63,18 @@ public class SkillController extends WebMvcConfigurerAdapter {
             repository.delete(id);
         }
 
-        return "redirect:/skills";
+        return "redirect:/workers";
     }
 
     @PostMapping("/")
-    public String checkSkill(@Valid Skill skill, BindingResult bindingResult) {
+    public String checkWorker(@Valid Worker worker, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            return "skill/create";
+            return "worker/create";
         }
-        repository.save(new Skill(skill.getName()));
+        repository.save(new Worker(worker.getFirstName(), worker.getLastName()));
 
-        return "redirect:/skills";
+        return "redirect:/workers";
     }
 
 }
